72c897207e3a5626f3f7f3ab3117c027c8da4e33
Piers Rollinson <piers.rollinson@gmail.com>
2022-02-02 11:09:47 -0800

    Setup basic page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders vegas vickie', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/vegas vickie/i);
   expect(linkElement).toBeInTheDocument();
 });
