0de4344d98e1b4dfe328f318352ebf8ad1e35426
Piers Rollinson <piers.rollinson@gmail.com>
2022-06-12 15:16:33 -0700

    Revert "Remove MoralisProvider"
    This reverts commit b8339cd1f4ed87e9ebbaf58c2a26ec4182dbb580.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,50 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { MoralisProvider } from 'react-moralis';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 
+// TODO: Extract into config file
+const moralisConfig = {
+  hardhat: {
+    serverUrl: 'https://xsty5vsxh3wr.usemoralis.com:2053/server',
+    appId: 'FMa48srGU8WBnvEDSdPVq19EzJilpf3zetM4Gpi1',
+  },
+  rinkeby: {
+    serverUrl: 'https://hejknhfi7e6d.usemoralis.com:2053/server',
+    appId: 'FuDzzNJqepkfhrBdsfYLYdy9RvsCYVBfgpriWdVs',
+  },
+  mainnet: {
+    serverUrl: '://hejknhfi7e6d.usemoralis.com:2053/server',
+    appId: '',
+  }
+}
+
+let currentMoralisConfig = null
+switch(process.env.REACT_APP_NODE_ENV) {
+  case 'development':
+    currentMoralisConfig = moralisConfig.hardhat;
+  break;
+  case 'production':
+    currentMoralisConfig = moralisConfig.mainnet;
+  break;
+  case 'staging':
+    currentMoralisConfig = moralisConfig.rinkeby;
+  break;
+  default:
+    currentMoralisConfig = moralisConfig.hardhat;
+  break;
+}
+
 ReactDOM.render(
   <React.StrictMode>
+    <MoralisProvider serverUrl={currentMoralisConfig.serverUrl} appId={currentMoralisConfig.appId}>
       <BrowserRouter>
         <ScrollToTop />
         <App />
       </BrowserRouter>
+    </MoralisProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
