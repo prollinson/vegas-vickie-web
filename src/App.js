152b6dd0c32113b4ff9dd0bac576e56cd9660b48
Piers Rollinson <piers.rollinson@gmail.com>
2022-06-14 13:30:41 -0700

    Add redirect to Circa

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Vegas Vickie NFT - Coming soon
+        Vegas Vickie NFT - Redirecting to https://www.circalasvegas.com/vegas-vickie-nft/
       </header>
+      <Redirect />
     </div>
   );
+}
+
+function Redirect() {
+  window.location.replace('https://www.circalasvegas.com/vegas-vickie-nft/');
+
+  return null;
 }
 
 export default App;
